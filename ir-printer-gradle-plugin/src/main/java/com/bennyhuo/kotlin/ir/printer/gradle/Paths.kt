c04ccafcc204706a50dd2708bf81c1f6cc620820
bennyhuo <bennyhuo@kotliner.cn>
2025-06-23 06:38:40 +0800

    Separate build type for llvm ir output.

--- ir-printer-gradle-plugin/src/main/java/com/bennyhuo/kotlin/ir/printer/gradle/Paths.kt
+++ ir-printer-gradle-plugin/src/main/java/com/bennyhuo/kotlin/ir/printer/gradle/Paths.kt
@@ -3,17 +3,26 @@ package com.bennyhuo.kotlin.ir.printer.gradle
 import java.io.File
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBinary
 
 /**
  * Created by benny.
  */
+fun Project.output(extension: PrinterExtension, binary: NativeBinary, path: String): String {
+    return output(extension, path, binary.buildType.getName(), binary.target.targetName)
+}
+
 fun Project.output(extension: PrinterExtension, target: KotlinTarget, path: String): String {
+    return output(extension, path, target.targetName)
+}
+
+fun Project.output(extension: PrinterExtension, vararg paths: String): String {
     val userConfiguredOutputDir = extension.outputDir
     return if (userConfiguredOutputDir.isNullOrBlank()) {
         layout.buildDirectory.file(
-            listOf("outputs", "kotlin", path, target.targetName).joinToString(File.separator)
+            listOf("outputs", "kotlin", *paths).joinToString(File.separator)
         ).get().asFile.path
     } else {
-        File(userConfiguredOutputDir, listOf(path, target.targetName).joinToString(File.separator)).path
+        File(userConfiguredOutputDir, paths.joinToString(File.separator)).path
     }
 }
