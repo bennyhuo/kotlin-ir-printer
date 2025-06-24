bc9613f75349d0c75b7368c1b62feecd0cf727fc
bennyhuo <bennyhuo@kotliner.cn>
2025-06-24 08:52:52 +0800

    Print optimized IR for wasm targets.

--- ir-printer-compiler/src/main/java/com/bennyhuo/kotlin/ir/printer/compiler/IrPrinterForOptimized.kt
+++ ir-printer-compiler/src/main/java/com/bennyhuo/kotlin/ir/printer/compiler/IrPrinterForOptimized.kt
@@ -3,6 +3,7 @@
 package com.bennyhuo.kotlin.ir.printer.compiler
 
 import com.bennyhuo.kotlin.ir.printer.compiler.options.Options
+import org.jetbrains.kotlin.backend.common.CommonBackendContext
 import org.jetbrains.kotlin.backend.common.phaser.Action
 import org.jetbrains.kotlin.backend.common.phaser.ActionState
 import org.jetbrains.kotlin.backend.common.phaser.BeforeOrAfter
@@ -21,6 +22,7 @@ import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 fun registerPrinterForOptimizedIr() {
     registerPrinterForJvmTargets()
     registerPrinterForNativeTargets()
+    registerPrinterForWasmTargets()
 }
 
 private fun getIrPrinterForJvmTargets(outputDirOptPath: String): Action<IrModuleFragment, JvmBackendContext> =
@@ -34,7 +36,7 @@ private fun registerPrinterForJvmTargets() {
     try {
         val actionsField = NamedCompilerPhase::class.java.getDeclaredField("actions")
         actionsField.isAccessible = true
-        @Suppress("UNCHECKED_CAST") 
+        @Suppress("UNCHECKED_CAST")
         val actions = actionsField.get(jvmLoweringPhases) as MutableSet<Action<IrModuleFragment, JvmBackendContext>>
         actionsField.set(jvmLoweringPhases, actions + getIrPrinterForJvmTargets(Options.outputDirOptPath()))
 
@@ -64,5 +66,29 @@ private fun registerPrinterForNativeTargets() {
         logger.info("The printer of optimized IR for native targets is ignored.")
     } catch (t: Throwable) {
         logger.warn("Failed to register the printer of optimized IR for native targets: $t")
+    }
+}
+
+private fun getIrPrinterForWasmTargets(outputDirOptPath: String): Action<IrModuleFragment, CommonBackendContext> =
+    fun(_: ActionState, data: IrModuleFragment, _: CommonBackendContext) {
+        printIr(data, outputDirOptPath)
+    }
+
+private fun registerPrinterForWasmTargets() {
+    try {
+        val preactionsField = SimpleNamedCompilerPhase::class.java.getDeclaredField("preactions")
+        preactionsField.isAccessible = true
+        @Suppress("UNCHECKED_CAST")
+        val wasmLoweringPhasesKtClass = Class.forName("org.jetbrains.kotlin.backend.wasm.WasmLoweringPhasesKt")
+        val validateIrAfterLoweringField = wasmLoweringPhasesKtClass.getDeclaredField("validateIrAfterLowering")
+        validateIrAfterLoweringField.isAccessible = true
+        val preactions = preactionsField.get(validateIrAfterLoweringField.get(null)) as MutableSet<Action<IrModuleFragment, CommonBackendContext>>
+        preactions.add(getIrPrinterForWasmTargets(Options.outputDirOptPath()))
+
+        logger.warn("Register the printer of optimized IR for wasm targets.")
+    } catch (_: NoClassDefFoundError) {
+        logger.info("The printer of optimized IR for wasm targets is ignored.")
+    } catch (t: Throwable) {
+        logger.warn("Failed to register the printer of optimized IR for wasm targets: $t")
     }
 }
